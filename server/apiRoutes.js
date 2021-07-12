8eb8b405d9475d028d1f3bfdaa22c7301dafbc78
Jacob Melnick <jake.r.emelnick@gmail.com>
2021-07-12 15:30:54 -0700

    add api routes

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -1,24 +1,56 @@
-const express = require('express');
-const cloudinary = require('cloudinary');
-const config = require('../config');
-
+const express = require("express");
+const cloudinary = require("cloudinary");
+const axios = require("axios");
+const config = require("../config");
 const router = express.Router();
-
+const handleResponse = require("./helpers/handleResponse");
 cloudinary.config(config.cloudinaryCreds);
 
-router.post('/image-upload', (req, res) => {
+router.post("/image-upload", (req, res) => {
   let values = Object.values(req.files);
 
   if (Array.isArray(values[0])) {
-    // eslint-disable-next-line prefer-destructuring
+    eslint-disable-next-line prefer-destructuring
     values = values[0];
   }
 
-  const promises = values.map((image) => cloudinary.uploader.upload(image.path));
+  const promises = values.map((image) =>
+    cloudinary.uploader.upload(image.path)
+  );
 
-  Promise
-    .all(promises)
-    .then((results) => res.json(results));
+  Promise.all(promises).then((results) => res.json(results));
+});
+
+router.get('/recipe/:id', (req, res) => {
+  const { params } = req;
+  axios({
+    url: `https://api.spoonacular.com/recipes/${params.id}/information`,
+    params: {
+      apiKey: 'ae8e71d80acd4e2a8028b74af00ac94d',
+      id: 'id',
+    },
+  })
+    .then((response) => {
+      handleResponse(res, 200, response.data);
+    })
+    .catch((err) => {
+      handleResponse(res, 400, err);
+    });
+});
+router.get('/ingredients', (req, res) => {
+  const { query } = req;
+  axios({
+    url: `https://api.spoonacular.com/food/ingredients/search?query=${query.query}`,
+    params: {
+      apiKey: 'ae8e71d80acd4e2a8028b74af00ac94d',
+    },
+  })
+    .then((response) => {
+      handleResponse(res, 200, response.data);
+    })
+    .catch((err) => {
+      handleResponse(res, 400, err);
+    });
 });
 
 module.exports = router;
