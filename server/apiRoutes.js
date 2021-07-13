ccd12c5b2b5426651fb3a65900d375ee2ce1c8f3
Joseph Balaoing <joseph.balaoing1@yahoo.com>
2021-07-12 17:04:01 -0700

    Refactored mock data to its own file, adjusted api params

--- server/apiRoutes.js
+++ server/apiRoutes.js
@@ -3,233 +3,37 @@ const express = require('express');
 const cloudinary = require('cloudinary');
 // const config = require('../config');
 const axios = require('axios');
+const mockIngredients = require('./mockIngredientsList');
 
 const router = express.Router();
 
 router.get('/recipes', (req, res) => {
 //  console.log('GET REQUEST FOR RECIPES');
 //  Mock Ingredients
-  const mainIngredients = ['pasta', 'potato'];
-  const allIngredients = `green bean
-  red chile powder
-  turmeric
-  pumpernickel
-  baguette
-  baking powder
-  unsweetened chocolate
-  shish
-  couscous
-  arugula
-  nosh
-  muffin
-  jelly
-  rye
-  basil
-  sea cucumber
-  parsley
-  bok choy
-  garlic
-  breadcrumb
-  walnut
-  papaya
-  mustard
-  tonic water
-  fennel seed
-  potato
-  pancake
-  marionberry
-  chutney
-  marshmallow
-  apple butter
-  macaroni
-  coconut ice cream
-  hot sauce
-  persimmon
-  cannellini beans
-  rice
-  aspic
-  caramel
-  rosemary
-  cornflake
-  kidney bean
-  watercress
-  strawberry
-  barbecue
-  lemon zest
-  tortilla
-  pickle
-  Marsala
-  margarine
-  pear
-  coconut milk
-  almond paste
-  summer squash
-  almond butter
-  cremini mushrooms
-  peaches
-  vinegar
-  tartar sauce
-  toffee
-  applesauce
-  cranberry
-  lemonade
-  curry leaves
-  black olive
-  popcorn
-  rice paper
-  pepper
-  date
-  waffle
-  cider
-  panko bread crumbs
-  celery
-  pancetta
-  squash
-  pesto
-  hoisin sauce
-  maple syrup
-  remoulade
-  watermelon
-  flax seed
-  hash brown
-  sorbet
-  tamale
-  coconut cream
-  chip
-  pumpkin seed
-  coleslaw
-  water
-  apple cider vinegar
-  tangerine
-  almond milk
-  zucchini
-  chickpeas
-  lentil
-  cornstarch
-  tomato juice
-  potato chip
-  provolone
-  cooking wine
-  eggplant
-  wine
-  chives
-  kohlrabi
-  radish
-  legume
-  tomatillo
-  rose water
-  onion powder
-  baking soda
-  cashew nut
-  leek
-  durian
-  red cabbage
-  wheat
-  raspberry
-  bouillon
-  parsnip
-  navy beans
-  alfalfa sprout
-  orange
-  lima bean
-  date sugar
-  melon
-  corn
-  barley
-  cornmeal
-  curry powder
-  cantaloupe
-  chocolate
-  mustard seeds
-  pineapple
-  pea beans
-  oil
-  ginger ale
-  chipotle pepper
-  raisin
-  lemon
-  sweet pepper
-  cumin
-  citrus
-  brussels sprout
-  pomelo
-  five-spice powder
-  Tabasco sauce
-  dried leek
-  tapioca
-  bagel
-  sour cream
-  sherbet
-  oats
-  rum
-  flour
-  spud
-  quinoa
-  snow pea
-  peas
-  truffle
-  oatmeal
-  asparagus
-  yeast
-  noodle
-  pita bread
-  toast
-  poppy seed
-  mint
-  tomato puree
-  lettuce
-  sweet chilli sauce
-  seaweed
-  carrot
-  allspice
-  blackberry
-  strudel
-  vegemite
-  peanut butter
-  plum
-  kale
-  romaine lettuce
-  mango
-  wine vinegar
-  rice vinegar
-  succotash
-  prune
-  guacamole
-  heavy cream
-  white bean
-  water chestnut
-  coconut yogurt
-  pecans
-  horseradish
-  acorn squash
-  licorice
-  syrup
-  apple
-  acorn
-  pretzel
-  maraschino cherry
-  soy milk
-  blueberry`.split('\n  ');
+//  const mainIngredients = ['pasta', 'potato'];
+  const allIngredients = req.body.ingredients || mockIngredients;
   console.log(allIngredients);
 
   const matchingRecipes = [];
   const promises = [];
 
-  for (let i = 0; i < mainIngredients.length; i++) {
+  let i = 0;
+  while (matchingRecipes.length < 20 && i < 1) {
     promises.push(
       axios({
         url: 'https://api.spoonacular.com/recipes/complexSearch',
         params: {
-          apiKey: '08ef9a2f826c4772adcf01e60b256174',
-          includeIngredients: mainIngredients[i],
-          number: 50,
+          apiKey: 'c42a21f8ef924ce0a56976daa8bd437b',
+          // includeIngredients: mainIngredients[i],
+          number: 100,
           fillIngredients: true,
           ignorePantry: true,
+          offset: i++,
         },
       })
         // eslint-disable-next-line no-loop-func
         .then((recipes) => {
-          console.log(recipes.data.results);
+          //console.log(recipes.data.results);
           for (let j = 0; j < recipes.data.results.length; j++) {
             let haveAllIngredients = true;
             const recipeIngredients = recipes.data.results[j].missedIngredients;
---
+++ server/mockIngredientsList.js
@@ -0,0 +1,500 @@
+module.exports = `watermelons
+passion fruit
+angelica
+broth
+applesauce
+Mandarin oranges
+zinfandel wine
+mustard
+turkeys
+creme fraiche
+marmalade
+duck
+heavy cream
+beef
+coffee
+papayas
+chai
+jelly beans
+orange peels
+romaine lettuce
+sushi
+snow peas
+chili powder
+chutney
+melons
+capers
+fennel seeds
+alligator
+corned beef
+won ton skins
+barbecue sauce
+aquavit
+yogurt
+breadcrumbs
+vanilla
+capers
+habanero chilies
+black-eyed peas
+prosciutto
+capers
+sesame seeds
+bruschetta
+brunoise
+celery
+pea beans
+honeydew melons
+crabs
+onions
+butter
+cactus
+sunflower seeds
+graham crackers
+ketchup
+sazon
+jelly beans
+chestnuts
+octopus
+lemon Peel
+sunflower seeds
+olive oil
+tea
+almond butter
+wine vinegar
+raspberries
+lentils
+sweet chili sauce
+passion fruit
+chicken
+asiago cheese
+sweet potatoes
+cabbage
+black-eyed peas
+Canadian bacon
+broth
+dill
+black beans
+grapes
+monkfish
+moo shu wrappers
+allspice
+cottage cheese
+sunflower seeds
+peas
+granola
+wild rice
+pinto beans
+brown rice
+avocados
+honeydew melons
+chicory
+water chestnuts
+truffles
+strawberries
+vanilla
+duck
+lobsters
+curry leaves
+corn flour
+tomato puree
+guavas
+granola
+mascarpone
+powdered sugar
+sherry
+cornmeal
+rose water
+cabbage
+bacon
+alfredo sauce
+pancetta
+anchovies
+wine vinegar
+habanero chilies
+gelatin
+pumpkin seeds
+Havarti cheese
+swordfish
+acorn squash
+red beans
+chicken liver
+cream of tartar
+ricotta cheese
+prunes
+marmalade
+rosemary
+ginger ale
+cream
+tomato sauce
+cremini mushrooms
+blackberries
+onions
+cranberries
+beans
+red pepper flakes
+peas
+summer squash
+raw sugar
+moo shu wrappers
+coffee
+limes
+quail
+bananas
+chicken liver
+red chile powder
+pineapples
+almonds
+black beans
+alligator
+bananas
+brown rice
+peas
+vanilla bean
+apple butter
+alligator
+venison
+wine
+pesto
+vegemite
+coconut milk
+tea
+almond paste
+creme fraiche
+vermouth
+artichokes
+walnuts
+cocoa powder
+flour
+curry leaves
+chaurice sausage
+sweet potatoes
+dill
+bruschetta
+focaccia
+chaurice sausage
+baguette
+almond butter
+blueberries
+beets
+beef
+kumquats
+aquavit
+raisins
+veal
+anchovies
+bacon
+allspice
+tomato sauce
+hash browns
+lamb
+brown sugar
+paprika
+crayfish
+oatmeal
+cheddar cheese
+eggplants
+ancho chile peppers
+salmon
+almond butter
+lemon grass
+succotash
+chutney
+barbecue sauce
+mussels
+guavas
+raw sugar
+kumquats
+fennel seeds
+anchovies
+corn flour
+garlic powder
+bagels
+cranberries
+coriander
+jelly beans
+bouillon
+breadfruit
+cottage cheese
+beans
+bean threads
+wasabi
+apple pie spice
+sea cucumbers
+pinto beans
+salsa
+ham
+beef
+pears
+figs
+crayfish
+radishes
+Cappuccino Latte
+cherries
+black olives
+snapper
+grouper
+venison
+pig's feet
+prawns
+bourbon
+french fries
+chocolate
+soymilk
+cornstarch
+okra
+sesame seeds
+bagels
+custard
+split peas
+grapefruits
+swordfish
+red pepper flakes
+couscous
+chutney
+sour cream
+romaine lettuce
+Tabasco sauce
+steak
+baking powder
+berries
+crabs
+chili sauce
+lima beans
+spearmint
+chili sauce
+chile peppers
+feta cheese
+gouda
+bacon
+pecans
+parsley
+honey
+pears
+bacon grease
+hash browns
+allspice
+half-and-half
+anchovies
+chicken
+dumpling
+chicory
+plums
+Irish cream liqueur
+vanilla
+rhubarb
+bouillon
+five-spice powder
+tomato puree
+lettuce
+canola oil
+English muffins
+kidney beans
+Mandarin oranges
+cookies
+cantaloupes
+bean threads
+kumquats
+bruschetta
+rice vinegar
+sage
+prawns
+snapper
+zinfandel wine
+apple pie spice
+custard
+pine nuts
+cauliflower
+olive oil
+curry paste
+rose water
+cilantro
+grouper
+pink beans
+baking powder
+breadfruit
+lemons
+cantaloupes
+cashew nut
+corn
+milk
+steak
+tortillas
+zest
+cumin
+English muffins
+rosemary
+couscous
+tomato puree
+watermelons
+English muffins
+rice wine
+cinnamon
+garlic powder
+brazil nuts
+barley
+caviar
+Mandarin oranges
+lima beans
+heavy cream
+grits
+pesto
+carrots
+powdered sugar
+tuna
+parsley
+water
+eggplants
+peaches
+alligator
+kiwi
+soymilk
+custard
+spinach
+spinach
+cayenne pepper
+amaretto
+kidney beans
+bacon
+sauerkraut
+asiago cheese
+almond extract
+salsa
+cookies
+cream of tartar
+cucumbers
+baguette
+walnuts
+sage
+spearmint
+watermelons
+grapefruits
+caviar
+anchovy paste
+angelica
+raw sugar
+cherries
+garlic powder
+sour cream
+pinto beans
+pea beans
+sweet peppers
+pine nuts
+almond butter
+tomato juice
+navy beans
+tonic water
+okra
+vegemite
+eggs
+moo shu wrappers
+berries
+plum tomatoes
+hamburger
+beef
+vinegar
+broth
+brussels sprouts
+chives
+pico de gallo
+ice cream
+brown rice
+french fries
+cashew nut
+onion powder
+cannellini beans
+coconuts
+nectarines
+grapefruits
+lima beans
+pumpkins
+pig's feet
+hot sauce
+pasta
+onions
+lobsters
+raisins
+powdered sugar
+onions
+swordfish
+rhubarb
+caviar
+cooking wine
+tea
+water chestnuts
+tofu
+grits
+bagels
+balsamic vinegar
+cottage cheese
+moo shu wrappers
+brussels sprouts
+herring
+strawberries
+oranges
+Irish cream liqueur
+almond paste
+radishes
+chestnuts
+canola oil
+sesame seeds
+red chile powder
+huckleberries
+chile peppers
+rosemary
+onion powder
+broth
+almond extract
+water chestnuts
+dates
+prosciutto
+papayas
+pancetta
+kiwi
+cilantro
+moo shu wrappers
+walnuts
+monkfish
+rice vinegar
+powdered sugar
+red chile powder
+nectarines
+curry leaves
+chambord
+sunflower seeds
+mushrooms
+pomegranates
+anchovies
+feta cheese
+Worcestershire sauce
+parsley
+ale
+onion powder
+black-eyed peas
+plantains
+ancho chile peppers
+split peas
+sour cream
+paprika
+white chocolate
+cloves
+barbecue sauce
+grapes
+cornstarch
+pheasants
+navy beans
+cucumbers
+grouper
+turnips
+ricotta cheese
+fish sauce
+sour cream
+bok choy
+sushi
+soy sauce
+brunoise
+liver
+okra
+potatoes
+rosemary
+jack cheese
+balsamic vinegar`.split('\n');
